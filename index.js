27ebd81f7d4078180378db34b7ca7d61dc77541d
Jesse Li <jesse.li286@gmail.com>
2022-12-04 18:56:21 -0500

    add team stats/standings

--- index.js
+++ index.js
@@ -3,20 +3,31 @@ const path = require('path');
 const axios = require('axios')
 const app = express();
 
-const hockey = require('./hockey')
+const hockey = require('./logic/hockey')
 
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // route handler
-app.get('/', async (req, res) => {
+app.get('/upcoming', async (req, res) => {
     // DATA FROM LIVE GAMES. CAN YOU SAVE IT? BUT ONLY IF YOURE REPEATEDLY CALLING IT....
     // each time somebody comes to the website just update it to show the live game. guess cant really save it....
     // on game day compare each teams stats? like maple leafs versus their opponent
     var response = await hockey.getUpcomingSchedule();
     var games = response.data.dates[0].games;
-    res.render('test.ejs', {title:"NHL DATA",games:games});
+    res.render('games.ejs', {title:"NHL DATA",games:games});
+
+});
+
+app.get('/team/:id', async (req, res) => {
+    // DATA FROM LIVE GAMES. CAN YOU SAVE IT? BUT ONLY IF YOURE REPEATEDLY CALLING IT....
+    // each time somebody comes to the website just update it to show the live game. guess cant really save it....
+    // on game day compare each teams stats? like maple leafs versus their opponent
+    var teamId = req.params.id;
+    var teamStats = await hockey.getTeamStats(teamId);
+    console.log(teamStats)
+    res.render('teamStats.ejs', {title:"TEAM STATS/STANDINGS",teamStats:teamStats});
 
 });
 
--- logic/hockey.js
+++ logic/hockey.js
@@ -4,7 +4,13 @@ const getUpcomingSchedule = async () => {
     return await axios.get("https://statsapi.web.nhl.com/api/v1/schedule", { headers: { Accept: 'application/json', 'Accept-Encoding': 'identity' }, params: { trophies: true } });
 }
 
+const getTeamStats = async (id) => {
+  return await axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${id}/stats`, { headers: { Accept: 'application/json', 'Accept-Encoding': 'identity' }, params: { trophies: true } });
+}
+
+
 
   module.exports = {
-    getUpcomingSchedule
+    getUpcomingSchedule,
+    getTeamStats
   }
