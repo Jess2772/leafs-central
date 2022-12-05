4623975d3f05718d387ce6e3a9085a2c0b14f6c5
Jesse Li <jesse.li286@gmail.com>
2022-12-05 01:00:41 -0500

    add mongodb cluster

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios')
 const app = express();
+const async = require('async')
 
-const hockey = require('./logic/hockey')
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://jboy2772:jimmyjrbestdood27@cluster27.nv3qhde.mongodb.net/nhl_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+const hockey = require('./logic/hockey');
+const Game = require('./models/game');
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -21,13 +27,17 @@ app.get('/upcoming', async (req, res) => {
     // page for leafs news?
     // can have a dynamic endpoint that takes in a gameid, so all display games route to that one webpage?
     // when searching for a game in database, will need to associate the game with a key, like key value pair in order to find the game
-    
+
     var response = await hockey.getUpcomingSchedule();
     var games = response.data.dates[0].games;
     var test = await hockey.getLeafsUpcomingSchedule();
-    console.log(test.data.totalGames);
     res.render('games.ejs', {title:"NHL DATA",games:games});
 
+    const query = {"home_goals":3};
+    const gg = Game.find(query);
+    const result1 = await gg.exec();
+    console.log(result1)
+    
 });
 
 app.get('/team/:id', async (req, res) => {
---
+++ models/game.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const gameDetailsSchema = new Schema({
+    _someId: Schema.Types.ObjectId,
+    home_team: {type: String, required: true},
+    home_goals: {type: Number, required: true},
+    away_team: {type: String, required: true},
+    away_goals: {type: Number, required: true},
+    date: {type: Date, required: true},
+    home_scorers: [String],
+    away_scorers: [String]
+});
+
+module.exports = mongoose.model("Game", gameDetailsSchema);
+
+
+
