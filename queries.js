fe909784d4db3874f677f823c73e7d535422cb0d
Jesse Li <jesse.li286@gmail.com>
2022-12-08 19:13:17 -0500

    add upcoming game cards

--- queries.js
+++ queries.js
@@ -8,13 +8,6 @@ async function findGameById(game_id) {
 
 }
 
-// async function findUpcomingGames(game_id) {
-//     var currentDate = dateFormat(new Date(), "yyyy-mm-dd");
-//     var query = {"game_id":game_id}; // this is how to query based on size of array
-//     var entries = Game.find(query);
-//     var result = await entries.exec();
-
-// }
 
 
 module.exports = {
